4e7f01f3ee2d46f3e364db4e5cb4ea437d22204d
Administrator <Administrator@LX-201506012205>
2016-08-19 18:06:25 +0800

    添加项目文件。

---
+++ MusicStore/Controllers/OrderController.cs
@@ -0,0 +1,25 @@
+﻿using MusicStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MusicStore.Controllers
+{
+    public class OrderController : Controller
+    {
+        MusicStoreEntity db = new MusicStoreEntity();
+        // GET: Order
+        public ActionResult Index()
+        {
+            var list = db.Orders.Where(p => p.Username == User.Identity.Name);
+            return View(list);
+        }
+        public ActionResult Details(int id)
+        {
+            var list = db.Orders.Single(p => p.OrderId==id);
+            return View(list);
+        }
+    }
+}
